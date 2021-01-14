7039bfe5973e1c6dfabbd3880c8fb3e3d8666660
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-01-14 17:35:15 +0700

    add router create update delete show all tag

--- routers/test.router.js
+++ routers/test.router.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const testController = require('../controllers/test.controller')
 
 router.get('/hello', testController.hello)
+router.post('/create_tag', testController.create_tag)
+router.get('/update_tag', testController.update_tag)
+router.get('/delete_tag', testController.delete_tag)
+router.get('/show_all_tag', testController.show_all_tag)
 
 module.exports = router
